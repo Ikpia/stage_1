7667ef35ce495b0fd81902800524c3fd65427d28
Ikpia <echukwuka97@gmail.com>
2024-07-02 02:06:47 +0100

    built controller

--- controller/endPointController.js
+++ controller/endPointController.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
 const axios = require("axios");
 const { IPinfoWrapper } = require("node-ipinfo");
-const TOKEN = "8fbc9748d086c6";
-const ipinfo = new IPinfoWrapper(TOKEN);
+const TOKEN = process.env.TOKEN;
+const API_KEY = process.env.API_KEY;
+const ipinfo = new IPinfoWrapper("8fbc9748d086c6");
 
 const controller = async (req, res) => {
   try {
@@ -11,10 +13,21 @@ const controller = async (req, res) => {
     const result = await ipinfo.lookupIp(ip);
     console.log(result);
 
+    const location = await axios.get(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${result.city},&appid=${API_KEY}`
+    );
+    console.log(location.data);
+    console.log(location.data[0].lat);
+
+    const temperature = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${location.data[0].lat}&lon=${location.data[0].lon}&appid=${API_KEY}&units=metric`
+    );
+    console.log(temperature.data.main.temp);
+
     const response = {
       client_ip: `${ip}`, // The IP address of the requester
       location: `${result.city}`, // The city of the requester
-      greeting: `Hello, ${visitor_name}!, the temperature is 11 degrees Celcius in ${result.city}`,
+      greeting: `Hello, ${visitor_name}!, the temperature is ${temperature.data.main.temp} degrees Celcius in ${result.city}`,
     };
     res.status(200).json(response);
   } catch (error) {
